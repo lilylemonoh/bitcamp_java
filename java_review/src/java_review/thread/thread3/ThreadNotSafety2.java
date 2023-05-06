bff8d02774266aa21d648236538a8e7edbebfe8a
yeonsuoh <lilylemonoh@naver.com>
2023-05-06 17:26:41 +0900

    오연수 : 자바 리뷰 파일 업로드

---
+++ java_review/src/java_review/thread/thread3/ThreadNotSafety2.java
@@ -0,0 +1,32 @@
+package java_review.thread.thread3;
+
+public class ThreadNotSafety2 extends Thread{
+
+	static int share;
+	
+	public static void main(String[] args) {
+		ThreadNotSafety2 t1 = new ThreadNotSafety2();
+		ThreadNotSafety2 t2 = new ThreadNotSafety2();
+		
+		t1.start();
+		t2.start();
+
+	}
+	
+	// 쓰레드 안전을 위해서 사용하는 메서드
+	public synchronized static void sharePlus() {
+		System.out.println(share++);
+	}
+
+	public void run() {
+		for(int count = 0; count < 10; count++) {
+			sharePlus();
+			try {
+				sleep(10);
+			}catch (InterruptedException e) {
+				
+			}
+		}
+		
+	}
+}
